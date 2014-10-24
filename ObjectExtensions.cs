9368c868c304b8c4e57469a6868f225cbd3c6ab6
rageit <rejanlikesraisin@hotmail.com>
2014-10-24 09:26:39 -0400

    Update ObjectExtensions.cs
    Formatting

--- ObjectExtensions.cs
+++ ObjectExtensions.cs
@@ -20,26 +20,36 @@ namespace System
         }
         private static Object InternalCopy(Object originalObject, IDictionary<Object, Object> visited)
         {
-            if (originalObject == null) return null;
+            if (originalObject == null) 
+                return null;
+
             var typeToReflect = originalObject.GetType();
-            if (IsPrimitive(typeToReflect)) return originalObject;
-            if (visited.ContainsKey(originalObject)) return visited[originalObject];
-            if (typeof(Delegate).IsAssignableFrom(typeToReflect)) return null;
-            var cloneObject = CloneMethod.Invoke(originalObject, null);
+            
+            if (IsPrimitive(typeToReflect)) 
+                return originalObject;
+
+            if (visited.ContainsKey(originalObject)) 
+                return visited[originalObject];
+
+            if (typeof(Delegate).IsAssignableFrom(typeToReflect)) 
+                return null;
+
+            var cloneobject = CloneMethod.Invoke(originalObject, null);
             if (typeToReflect.IsArray)
             {
                 var arrayType = typeToReflect.GetElementType();
                 if (IsPrimitive(arrayType) == false)
                 {
-                    Array clonedArray = (Array)cloneObject;
+                    var clonedArray = (Array)cloneobject;
                     clonedArray.ForEach((array, indices) => array.SetValue(InternalCopy(clonedArray.GetValue(indices), visited), indices));
                 }
-
             }
-            visited.Add(originalObject, cloneObject);
-            CopyFields(originalObject, visited, cloneObject, typeToReflect);
-            RecursiveCopyBaseTypePrivateFields(originalObject, visited, cloneObject, typeToReflect);
-            return cloneObject;
+
+            visited.Add(originalObject, cloneobject);
+            CopyFields(originalObject, visited, cloneobject, typeToReflect);
+            RecursiveCopyBaseTypePrivateFields(originalObject, visited, cloneobject, typeToReflect);
+
+            return cloneobject;
         }
 
         private static void RecursiveCopyBaseTypePrivateFields(object originalObject, IDictionary<object, object> visited, object cloneObject, Type typeToReflect)
@@ -51,17 +61,24 @@ namespace System
             }
         }
 
-        private static void CopyFields(object originalObject, IDictionary<object, object> visited, object cloneObject, Type typeToReflect, BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.FlattenHierarchy, Func<FieldInfo, bool> filter = null)
+        private static void CopyFields(object originalObject, IDictionary<object, object> visited, object cloneObject, Type typeToReflect,
+            BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.FlattenHierarchy, 
+            Func<FieldInfo, bool> filter = null)
         {
             foreach (FieldInfo fieldInfo in typeToReflect.GetFields(bindingFlags))
             {
-                if (filter != null && filter(fieldInfo) == false) continue;
-                if (IsPrimitive(fieldInfo.FieldType)) continue;
+                if (filter != null && filter(fieldInfo) == false) 
+                    continue;
+
+                if (IsPrimitive(fieldInfo.FieldType)) 
+                    continue;
+
                 var originalFieldValue = fieldInfo.GetValue(originalObject);
                 var clonedFieldValue = InternalCopy(originalFieldValue, visited);
                 fieldInfo.SetValue(cloneObject, clonedFieldValue);
             }
         }
+        
         public static T Copy<T>(this T original)
         {
             return (T)Copy((Object)original);
@@ -74,6 +91,7 @@ namespace System
         {
             return ReferenceEquals(x, y);
         }
+        
         public override int GetHashCode(object obj)
         {
             if (obj == null) return 0;
@@ -87,10 +105,15 @@ namespace System
         {
             public static void ForEach(this Array array, Action<Array, int[]> action)
             {
-                if (array.LongLength == 0) return;
-                ArrayTraverse walker = new ArrayTraverse(array);
-                do action(array, walker.Position);
-                while (walker.Step());
+                if (array.LongLength == 0) 
+                    return;
+                    
+                ArrayTraverse traverse = new ArrayTraverse(array);
+                
+                do 
+                {
+                    action(array, traverse.Position);
+                } while (traverse.Step());
             }
         }
 
